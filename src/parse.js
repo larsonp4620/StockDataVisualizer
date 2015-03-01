4ef3bade55e261241ffe12939859fc7793ffccb5
Jacob Knispel <knispeja@rose-hulman.edu>
2015-02-28 22:40:33 -0500

    Parse is Changed
    - All companies temporarily forced to start and end at the same time
    - Average prices of each time interval are now stored in
    averagePrices[], populated by parse()
    - We should implement a sliding window which calculates how much things
    have been changing

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,36 @@
 var names = [];
-var points = [];
+var startTime;
+var fieldDataLengths = [];
+var averagePrices = [];
 
 var parseJSON = function(name){
 	queryServer(name, function(unparsed){
 		var upd = unparsed.data[0].securityData;
+		var fieldDataLength = upd.fieldData.length;
+		
+		var updFix = upd.fieldData[0];
+		var thisStartTime = updFix.date;
+		
+		fieldDataLengths.push(fieldDataLength);
+		
+		for(var i=0; i<fieldDataLengths.length-1; i++){
+			if(fieldDataLengths[i] != fieldDataLength || startTime != thisStartTime){
+				fieldDataLengths.pop();
+				alert('This company does not exist in the same time frame.');
+				return;
+			}
+		}
+		
 		names.push(upd.security);	
-		points.push(upd.fieldData);
+		
+		var prices = [];
+		for(var j=0; j<fieldDataLength; j++){
+			var a = upd.fieldData[j].PX_LAST;
+			var b = upd.fieldData[j].OPEN;
+			prices.push((a+b)/2);
+		}
+		averagePrices.push(prices);
+		
 		alert(upd.security);
 		
 		if(names.length > 100)
