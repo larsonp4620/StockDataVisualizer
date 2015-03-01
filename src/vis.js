9e49cf8a98f00ab4ad192919c407f54e4e586ebf
larsonp4620 <peterlsm4620@gmail.com>
2015-02-28 22:32:39 -0500

    Added code to display circles. Change sizes
    No animation

--- src/vis.js
+++ src/vis.js
@@ -1,75 +1,39 @@
 var width = window.innerWidth,
     height = window.innerHeight;
 
-var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; }),
-    root = nodes[0];
-
-root.radius = 0;
-root.fixed = true;
-
-var force = d3.layout.force()
-    .gravity(0.05)
-    .charge(function(d, i) { return i ? 0 : -2000; })
-    .nodes(nodes)
-    .size([width, height]);
-
-force.start();
-
-var canvas = d3.select("body").append("canvas")
+var testArray = [[20, 30, 40], [10, 15, 10], [100, 50, 25]];
+var displayIndex=0;
+var color = ["#779ECB", "#B39EB5", "#F49AC2","#FFD1DC"];
+var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
+var singleArray = [];
+createCircles();
 
-var context = canvas.node().getContext("2d");
-
-force.on("tick", function(e) {
-  var q = d3.geom.quadtree(nodes),
-      i,
-      d,
-      n = nodes.length;
-
-  for (i = 1; i < n; ++i) q.visit(collide(nodes[i]));
-
-  context.clearRect(0, 0, width, height);
-  context.fillStyle = "steelblue";
-  context.beginPath();
-
-  for (i = 1; i < n; ++i) {
-    d = nodes[i];
-    context.moveTo(d.x, d.y);
-    context.arc(d.x, d.y, d.radius, 0, 2 * Math.PI);
-  }
-  context.fill();
-});
-
-canvas.on("mousemove", function() {
-  var p1 = d3.mouse(this);
-  root.px = p1[0];
-  root.py = p1[1];
-  force.resume();
-});
-
-function collide(node) {
-  var r = node.radius + 16,
-      nx1 = node.x - r,
-      nx2 = node.x + r,
-      ny1 = node.y - r,
-      ny2 = node.y + r;
-  return function(quad, x1, y1, x2, y2) {
-    if (quad.point && (quad.point !== node)) {
-      var x = node.x - quad.point.x,
-          y = node.y - quad.point.y,
-          l = Math.sqrt(x * x + y * y),
-          r = node.radius + quad.point.radius;
-      if (l < r) {
-        l = (l - r) / l * .5;
-        node.x -= x *= l;
-        node.y -= y *= l;
-        quad.point.x += x;
-        quad.point.y += y;
-      }
+function updateCircles() {
+    displayIndex++;
+    if (displayIndex >= testArray.length)
+        displayIndex = 0;
+    for (var i = 0; i < testArray.length; i++) {
+        singleArray[i].radius = testArray[i][displayIndex];
     }
-    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-  };
+    svg.selectAll("circle").data(singleArray);
+    svg.selectAll("circle").attr("r", function (d) { return d.radius; });
+}
+
+function createCircles() {
+    //var singleArray = [];
+    for (var i = 0; i < testArray.length; i++) {
+        singleArray.push({radius: testArray[i][0], x:width/testArray.length*((i+1)-1/2),y:height/2});
+    }
+    console.log(singleArray);
+
+    svg.selectAll("circle").data(singleArray)
+                            .enter().append("circle")
+    .attr("r", function (d) { return d.radius; })
+    .attr("cx", function (d) { return d.x; })
+    .attr("cy", function (d) { return d.y;})
+    .style("fill", function (d, i) { return color[i]; });
 }
 
 
