e5d0c2e82d40c2df78085b88c0d9595b709507db
Jacob Knispel <knispeja@rose-hulman.edu>
2015-03-01 01:53:43 -0500

    Revert "Work on predPrey"
    This reverts commit 824401fc02b9c43f67eb59eb189686c0b2e709f9.

--- src/vis.js
+++ src/vis.js
@@ -11,40 +11,28 @@ var dataArray = [];
 var animationStep = 100;
 var interval;
 var isLooping = false;
-var blockMovement = true;
 createCircles();
 
 function updateCircles() {
-    if (!blockMovement) {
-        displayIndex++;
-        if (displayIndex >= tempDispArray[0].length)
-            displayIndex = 0;
+    displayIndex++;
+    if (displayIndex >= tempDispArray[0].length)
+        displayIndex = 0;
 
-        for (var i = 0; i < tempDispArray.length; i++) {
-            dataArray[i].radius = tempDispArray[i][displayIndex];
-            console.log(tempDispArray[i][displayIndex]);
-        }
-        svg.selectAll("circle").data(dataArray);
-        svg.selectAll("circle").attr("r", function (d) { return d.radius; });
-    } else {
-        moveCircles();
-    }
-}
-
-function moveCircles() {
     for (var i = 0; i < tempDispArray.length; i++) {
-        dataArray[i].x += dataArray[i].dx;
-        dataArray[i].y += dataArray[i].dy;
+        dataArray[i].radius = tempDispArray[i][displayIndex];
+        console.log(tempDispArray[i][displayIndex]);
     }
-    svg.selectAll("circle").attr("cx", function (d) { return d.x; })
-    .attr("cy", function (d) { return d.y; });
+    svg.selectAll("circle").data(dataArray);
+    svg.selectAll("circle").attr("r", function (d) { return d.radius; });
 }
+
+
 
 function createCircles() {
     displayIndex = 0;
     dataArray = [];
     for (var i = 0; i < tempDispArray.length; i++) {
-        dataArray.push({ radius: tempDispArray[i][0], x: width / tempDispArray.length * ((i + 1) - 1 / 2), y: height / 2, dy: 1, dx: 1 });
+        dataArray.push({ radius: tempDispArray[i][0], x: width / tempDispArray.length * ((i + 1) - 1 / 2), y: height / 2, dy: 0, dx: 0 });
     }
     console.log(dataArray);
 
